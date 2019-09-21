3c9ba0a89c64cebaf53767accd2ec3511bb9364e
AR123456 <coloradoz99@gmail.com>
2019-09-21 07:01:55 -0600

    clearing out react stuff and addingcomponents

---
+++ src/components/Tetris.js
@@ -0,0 +1,23 @@
+import React from "react";
+// components
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
+
+const Tetris = () => {
+  return (
+    <div>
+      <Stage></Stage>
+      <aside>
+        <div>
+          <Display text="Score"></Display>
+          <Display text="Rows"></Display>
+          <Display text="Level"></Display>
+        </div>
+        <StartButton></StartButton>
+      </aside>
+    </div>
+  );
+};
+
+export default Tetris;
