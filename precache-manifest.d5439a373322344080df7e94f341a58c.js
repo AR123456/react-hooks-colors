9fe93c2832eef62c5d1938ee269805c91c117d0b
AR123456 <coloradoz99@gmail.com>
2019-09-21 07:20:34 -0600

    Updates

---
+++ precache-manifest.d5439a373322344080df7e94f341a58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4646ca81a2fb826feed40f62011a53d",
+    "url": "/react-hooks-tetris/index.html"
+  },
+  {
+    "revision": "cc53a68aadf4d4e53560",
+    "url": "/react-hooks-tetris/static/css/main.c7545cb6.chunk.css"
+  },
+  {
+    "revision": "19cab546face97cb94fe",
+    "url": "/react-hooks-tetris/static/js/2.b870ce23.chunk.js"
+  },
+  {
+    "revision": "cc53a68aadf4d4e53560",
+    "url": "/react-hooks-tetris/static/js/main.c3a1c4d7.chunk.js"
+  },
+  {
+    "revision": "b3a7429fe89300972a96",
+    "url": "/react-hooks-tetris/static/js/runtime~main.c596e34d.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-hooks-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-hooks-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
